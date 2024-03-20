e3b75c3ffa66d3761d24ef5302c4ae884ad1fa9c
johny11111 <125285264+johny11111@users.noreply.github.com>
2024-03-20 06:06:45 +0200

    remove  add del user route

--- router/routes/routeAuth.js
+++ router/routes/routeAuth.js
@@ -1,9 +1,33 @@
 const route = require('express').Router()
 const admin = require("../../config/firebase.config");
-const users = require('../../models/user');
+const user = require('../../models/user');
 const User = require("../../models/user");
 
 
+route.get('/login', async (req, res) => {
+    if (!req.headers.authorization) {
+        return res.status(500).send({ massage: "Invalid Token" })
+    }
+    const token = req.headers.authorization.split(" ")[1];
+
+    try {
+        const codeValue = await admin.auth().verifyIdToken(token);
+        if (!codeValue) {
+            return res.status(505).send({ massage: "unauthorized" })
+        } else {
+            const existUser = await User.findOne({ "userId": codeValue.user_id })
+
+            if (!existUser) {
+                return newUserValue(codeValue, req, res)
+
+            } else {
+                return updateUserData(codeValue, req, res)
+            }
+        }
+    } catch (err) {
+        return res.status(505).send({ message: err })
+    }
+})
 
 const newUserValue = async (codeValue, req, res) => {
     const newUser = new User({
@@ -14,7 +38,6 @@ const newUserValue = async (codeValue, req, res) => {
         emailVerified: codeValue.email_verified,
         role: "member",
         authTime: codeValue.auth_time,
-        playlist: [],
     })
     try {
         const savedUser = await newUser.save();
@@ -43,35 +66,9 @@ const updateUserData = async (codeValue, req, res) => {
     }
 }
 
-
-route.get('/login', async (req, res) => {
-    if (!req.headers.authorization) {
-        return res.status(500).send({ massage: "Invalid Token" })
-    }
-    const token = req.headers.authorization.split(" ")[1];
-
-    try {
-        const codeValue = await admin.auth().verifyIdToken(token);
-        if (!codeValue) {
-            return res.status(505).send({ massage: "unauthorized" })
-        } else {
-            const existUser = await User.findOne({ "userId": codeValue.user_id })
-
-            if (!existUser) {
-                return newUserValue(codeValue, req, res)
-
-            } else {
-                return updateUserData(codeValue, req, res)
-            }
-        }
-    } catch (err) {
-        return res.status(505).send({ message: err })
-    }
-})
-
 route.get('/getAllUsers', async (req, res) => {
     try {
-        const data = await users.find().sort({ createdAt: 1 });
+        const data = await user.find().sort({ createdAt: 1 });
 
         if (data.length > 0) {
             const leanData = data.map(doc => doc.toObject());
@@ -84,43 +81,22 @@ route.get('/getAllUsers', async (req, res) => {
     }
 })
 
-
-route.put("/updateRole/:userId", async (req, res) => {
-    const filter = { _id: req.params.userId }
+route.put("/updateRole/:userId" , async (req, res) => {
+    const filter = {_id: req.params.userId}
     const role = req.body.data.role
-    const option = {
-        upsert: true,
-        new: true,
-    };
+const option = {
+    upsert: true,
+    new: true,
+};
 
-    try {
-        const result = await users.findOneAndUpdate(filter, { role: role }, option);
-        res.status(200).send({ user: result })
-    } catch (error) {
-        return res.status(404).send({ success: false, msg: error });
-    }
+try {
+    const result = await user.findOneAndUpdate(filter , {role : role}, option);
+    res.status(200).send({user : result})
+} catch (error) {
+    return res.status(404).send({ success: false, msg: error });
+}
 
 })
-
-
-route.delete("/delete/:id", async (req, res) => {
-    try {
-        const filter = { _id: req.params.id };
-        if (filter) {
-            const result = await users.findOneAndDelete(filter , { _id: filter._id});
-         
-            if (result === null) {
-                return res.status(400).send({ success: false, msg: "user not found" });
-            } else {
-                return res.status(200).send({ success: true, msg: "user deleted successfully", data: result });
-            }
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).send({ success: false, msg: "Internal server error" });
-    }
-})
-
 
 
 module.exports = route
